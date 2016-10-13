3aba4191d010dffe8afd2fc979850bbf61412add
Brynn Mountain <BrynnMountain@Gmail.com>
2016-10-12 19:53:13 -0700

    Styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
         @foreach($user->boxes as $box)
             <div class="col-xs-12 col-sm-4">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $box->name }}</div>
+                    <div class="panel-heading">{{$box->name}}</div>
 
                     <div class="panel-body">
                         <div class="list-group">
